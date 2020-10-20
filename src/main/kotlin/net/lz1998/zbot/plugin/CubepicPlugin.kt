f9c78e0da2fb9a1bf44aeca229acb25cabe3cba8
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2020-10-21 03:31:24 +0800

    cubepic

---
+++ src/main/kotlin/net/lz1998/zbot/plugin/CubepicPlugin.kt
@@ -0,0 +1,50 @@
+package net.lz1998.zbot.plugin
+
+import net.lz1998.pbbot.alias.GroupMessageEvent
+import net.lz1998.pbbot.bot.Bot
+import net.lz1998.pbbot.bot.BotPlugin
+import net.lz1998.pbbot.utils.Msg
+import net.lz1998.zbot.aop.annotations.PrefixFilter
+import net.lz1998.zbot.aop.annotations.SwitchFilter
+import net.lz1998.zbot.config.ServiceConfig
+import org.springframework.stereotype.Component
+import java.net.URLEncoder
+
+@Component
+@SwitchFilter("cubepic")
+class CubepicPlugin : BotPlugin() {
+
+    @PrefixFilter(".")
+    override fun onGroupMessage(bot: Bot, event: GroupMessageEvent): Int {
+        val groupId = event.groupId
+        var rawMsg = event.rawMessage
+        if (rawMsg.startsWith("cubepic")) {
+            rawMsg = rawMsg.substring("cubepic".length).trim()
+            val rows = rawMsg.split("\n")
+            var alg = ""
+            val paramMap = mutableMapOf<String, String>()
+            rows.forEach {
+                if (it.contains("=")) {
+                    val split = it.split("=")
+                    if (split.size > 1) {
+                        paramMap[split[0]] = split[1]
+                    }
+                } else {
+                    alg = URLEncoder.encode(it, "utf-8")
+                }
+            }
+            val type = paramMap.getOrDefault("type", "alg")
+            paramMap.remove("type")
+            var imageUrl = "http://${ServiceConfig.vscube}/visualcube.php?fmt=png&${type}=${alg}"
+            paramMap.forEach {
+                val k = URLEncoder.encode(it.key, "utf-8")
+                val v = URLEncoder.encode(it.value, "utf-8")
+                imageUrl += "&$k=$v"
+            }
+            Msg.builder().image(imageUrl).sendToGroup(bot, groupId)
+            return MESSAGE_BLOCK
+        }
+        return super.onGroupMessage(bot, event)
+    }
+
+}
