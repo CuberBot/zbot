3d1e66bb6782abf4d7ee9f79b38de413e4e8069c
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2020-10-24 17:28:53 +0800

    event metrics

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-security")
--- src/main/kotlin/net/lz1998/zbot/plugin/LogPlugin.kt
+++ src/main/kotlin/net/lz1998/zbot/plugin/LogPlugin.kt
@@ -2,6 +2,7 @@ package net.lz1998.zbot.plugin
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import io.micrometer.core.instrument.Metrics
 import net.lz1998.pbbot.alias.*
 import net.lz1998.pbbot.bot.Bot
 import net.lz1998.pbbot.bot.BotPlugin
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Component
 // TODO
 @Component
 class LogPlugin : BotPlugin() {
+    val privateMessageCounter = Metrics.counter("event", "type", "private_message")
+    val groupMessageCounter = Metrics.counter("event", "type", "group_message")
 
     val log: Logger = LoggerFactory.getLogger(this::class.java)
     val objMapper = ObjectMapper()
@@ -46,6 +49,7 @@ class LogPlugin : BotPlugin() {
 
     override fun onGroupMessage(bot: Bot, event: GroupMessageEvent): Int {
 //        log.info("RECV GROUP_MSG {}", objMapper.writeValueAsString(event))
+        groupMessageCounter.increment()
         log.info("RECV GROUP MSG groupId={} userId={} msg={}", event.groupId, event.userId, event.rawMessage)
         return super.onGroupMessage(bot, event)
     }
@@ -60,6 +64,7 @@ class LogPlugin : BotPlugin() {
 
     override fun onPrivateMessage(bot: Bot, event: PrivateMessageEvent): Int {
 //        log.info("RECV PRIVATE_MSG {}", objMapper.writeValueAsString(event))
+        privateMessageCounter.increment()
         log.info("RECV PRIVATE MSG userId={} msg={}", event.userId, event.rawMessage)
         return super.onPrivateMessage(bot, event)
     }
