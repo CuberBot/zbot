3be586c16df53fc76b54ec582c1d08b5baf41cb3
lz1998 <875543533@qq.com>
2022-06-16 00:34:14 +0800

    nsfw

---
+++ src/main/kotlin/net/lz1998/zbot/plugin/NsfwPlugin.kt
@@ -0,0 +1,63 @@
+package net.lz1998.zbot.plugin
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import net.lz1998.pbbot.alias.GroupMessageEvent
+import net.lz1998.pbbot.bot.Bot
+import net.lz1998.pbbot.bot.BotPlugin
+import net.lz1998.zbot.aop.annotations.SwitchFilter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestTemplate
+
+
+@Component
+@SwitchFilter("nsfw")
+class NsfwPlugin : BotPlugin() {
+    @Autowired
+    lateinit var restTemplate: RestTemplate
+    var json: Json = Json {
+        isLenient = true
+        ignoreUnknownKeys = true
+    }
+
+    /**
+     * 收到群消息时调用此方法
+     *
+     * @param bot    机器人对象
+     * @param event 事件内容
+     * @return 是否继续处理下一个插件, MESSAGE_BLOCK表示不继续，MESSAGE_IGNORE表示继续
+     */
+    override fun onGroupMessage(bot: Bot, event: GroupMessageEvent): Int {
+        for (m in event.messageList) {
+            if (m.type != "image") {
+                continue
+            }
+            val headers = HttpHeaders()
+            headers.contentType = MediaType.MULTIPART_FORM_DATA
+
+            val map: MultiValueMap<String, String> = LinkedMultiValueMap()
+            map.add("url", m.dataMap["url"])
+
+            val request: HttpEntity<MultiValueMap<String, String>> =
+                HttpEntity<MultiValueMap<String, String>>(map, headers)
+            val respStr = restTemplate.postForObject(
+                "http://nsfw.zbot",
+                request,
+                String::class.java
+            )
+            val resp = json.decodeFromString<HashMap<String, Double>>(respStr.orEmpty());
+            val hentai = (resp["hentai"] ?: 0.0) * 100
+            if (hentai > 20.0) {
+                bot.sendGroupMsg(event.groupId, "hso\uD83D\uDE2E hentai detect:${String.format("%.2f", hentai)}%")
+                return MESSAGE_BLOCK
+            }
+        }
+        return MESSAGE_IGNORE
+    }
+}
